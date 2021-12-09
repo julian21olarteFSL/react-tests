7a634fca14127337d90c0f52d1da11a1e3cf7e59
Julian Olarte <jolarte@fullstacklabs.co>
2021-12-09 10:54:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+export const replaceCameWithSpaces = (colorName) => {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1')
+}
 
 function App() {
+
+  const [color, setColor] = useState('red')
+  const [checked, setChecked] = useState(false)
+  const newColor = color === 'red' ? 'blue' : 'red';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ backgroundColor: checked ? 'gray' : color, color: 'white' }}
+        onClick={() => setColor(newColor)}
+        disabled={checked}
+      >
+        Change to {newColor}
+      </button>
+      <input
+        type="checkbox" 
+        defaultChecked={checked}
+        id="change-button-checkbox"
+        onChange={() => setChecked(!checked)}
+      />
+      <label htmlFor='change-button-checkbox' >{checked ? 'Enable' : 'Disable'} button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,73 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App, { replaceCameWithSpaces } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Change the button color when clicked', () => {
+  render(<App />)
+
+  // find button by role and text
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+  
+  // test if button is red
+  expect(colorButton).toHaveStyle({
+    backgroundColor: 'red',
+    color: 'white'
+  })
+  
+  // click the button and test the new color blue and text
+  fireEvent.click(colorButton)
+  
+  expect(colorButton).toHaveStyle({
+    backgroundColor: 'blue'
+  })
+  
+  expect(colorButton.textContent).toBe('Change to red')
+
+  // click the button again
+  fireEvent.click(colorButton)
+
+  // button text should be 'Change to blue'
+  expect(colorButton.textContent).toBe('Change to blue')
+
+  // test if button is red
+  expect(colorButton).toHaveStyle({
+    backgroundColor: 'red'
+  })
 });
+
+test('Disable and enable button when checkbox is clicked', () => {
+  render(<App />)
+  
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' })
+  expect(colorButton).toBeEnabled()
+  
+  const checkBox = screen.getByRole('checkbox', { name: 'Disable button' })
+  expect(checkBox).not.toBeChecked()
+  
+  fireEvent.click(checkBox)
+  expect(checkBox).toBeChecked()
+  // expect(checkBox.textContent).toBe('Enable button')
+  expect(colorButton).toBeDisabled()
+  expect(colorButton).toHaveStyle({
+    backgroundColor: 'gray'
+  })
+
+  fireEvent.click(checkBox)
+  expect(checkBox).not.toBeChecked()
+  // expect(checkBox.textContent).toBe('Disable button')
+  expect(colorButton).toBeEnabled()
+  
+})
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCameWithSpaces('Red')).toBe('Red')
+  })
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCameWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCameWithSpaces('ThisShouldWorks')).toBe('This Should Works')
+  })
+})
